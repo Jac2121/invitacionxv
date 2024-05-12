62678d122222edcabe56b32e9d5da545dac8b0cb
Jac2121 <78768457+Jac2121@users.noreply.github.com>
2024-05-11 18:02:59 -0600

    Add files via upload

---
+++ sorpresa.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="es-Mx">
+<head>
+<link rel="icon" href="logo.png" width="50%" height="50%">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INVITACIÓN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <style>
+        body {
+            background-color: black; /* Color de fondo negro */
+        }
+        #textopresentacion{
+            color:#C38B99;
+            font-weight:bold;
+            font-size:500%;
+        }
+        #ubicacion{
+            color:#C38B99;
+            font-weight:bold;
+            font-size:250%;
+        }
+        #txtubicacion{
+            color:#C38B99;
+            font-weight:bold;
+            font-size:250%;
+        }
+    </style>
+</head>
+
+<body>
+    <!--  -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><img src="ic.png" alt="" width="150px" height="70px"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="portada.php">PORTADA</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link" href="presentacion.php">PRESENTACIÓN</a>
+        </li> -->
+
+        <li class="nav-item">
+          <a class="nav-link" href="dedicatoria.php">DEDICATORIA</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="agradecimiento.php">AGRADECIMIENTO</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="misa.php">MISA</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="salon.php">SALÓN</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" href="sorpresa.php">SORPRESA</a>
+        </li>
+
+
+    </div>
+  </div>
+</nav>
+    <!--  -->
+
+
+<center>
+<img src="https://i.pinimg.com/originals/6c/d1/0f/6cd10f3a31e9481ada8844dc8dc11cc3.jpg" alt="" width="80%" height="80%">
+
+<label id="textopresentacion">
+“No olvides 
+llevar tu regalo”
+</label>
+<br>
+<img src="https://pa1.aminoapps.com/6991/35de491b949165afa2f71219ddcf06d7d7a5b55ar1-320-320_00.gif" alt="">
+
+</center>
+</body>
+</html>
